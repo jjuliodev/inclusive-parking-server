9beb48b81203a42b967b29fb05fb6138844d1590
jjuliodev <jcjuriolli@proton.me>
2023-04-12 14:42:12 -0300

    chore: configure dependency dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3333, () => {
-     console.log("Hello World. Port 3333")
+app.listen(process.env.MYSQL_PORT, () => {
+     console.log(`Hello World. Port ${process.env.MYSQL_PORT}`)
 })
